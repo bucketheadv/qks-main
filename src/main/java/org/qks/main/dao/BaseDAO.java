d033b871e6f35e03b3107750fbe3e5709b81b61f
Sven <sven199109@gmail.com>
2025-03-10 16:19:55 +0800

    处理默认db注入的问题

---
+++ src/main/java/org/qks/main/dao/BaseDAO.java
@@ -0,0 +1,36 @@
+package org.qks.main.dao;
+
+import jakarta.persistence.EntityManager;
+
+import java.lang.reflect.ParameterizedType;
+
+public abstract class BaseDAO<T> {
+
+    protected EntityManager entityManager;
+
+    public BaseDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public T findById(Long id) {
+        return entityManager.find(getGenericClass(this.getClass(), 0), id);
+    }
+
+    public T updateById(T entity) {
+        return entityManager.merge(entity);
+    }
+
+    public T insert(T entity) {
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    public void deleteById(T entity) {
+        entityManager.remove(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Class<T> getGenericClass(Class<?> clazz, int index) {
+        return (Class<T>) ((ParameterizedType)clazz.getGenericSuperclass()).getActualTypeArguments()[index];
+    }
+}
--- src/main/java/org/qks/main/dao/UserDAO.java
+++ src/main/java/org/qks/main/dao/UserDAO.java
@@ -1,18 +1,15 @@
 package org.qks.main.dao;
 
-import io.quarkus.hibernate.orm.PersistenceUnit;
-import jakarta.inject.Inject;
+//import io.quarkus.hibernate.orm.PersistenceUnit;
 import jakarta.inject.Singleton;
 import jakarta.persistence.EntityManager;
 import org.qks.main.dao.po.UserPo;
 
 @Singleton
-public class UserDAO {
-    @Inject
-    @PersistenceUnit("second_db")
-    EntityManager entityManager;
-
-    public UserPo findById(Long id) {
-        return entityManager.find(UserPo.class, id);
+public class UserDAO extends BaseDAO<UserPo> {
+    public UserDAO(
+//            @PersistenceUnit("second_db")
+            EntityManager entityManager) {
+        super(entityManager);
     }
 }
